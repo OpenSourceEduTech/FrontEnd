674c0604b33328af618e54abc9da02cba898847f
youm <juliet0524@naver.com>
2023-04-13 17:22:25 +0900

    .

--- src/pages/Homework.js
+++ src/pages/Homework.js
@@ -1,10 +1,34 @@
 import Layout from "../components/Layout";
+import styled from "styled-components";
+
+const Body = styled.div`
+display:flex;
+  width: 100vw;
+  height: 100vh;
+  background-color: #FFFFFF;
+  align-items:columns;
+  position: fixed;
+  z-index:1;
+`;
+const Frame = styled.div`
+    width: 100%;
+    position: relative;
+    height:20%;
+    background-color: #000000;
+    z-index:2;
+`;
+
 const Homework = () => {
     return (
         <Layout>
-            
+            <Body>
+                <Frame>
+                    <a>hi</a>
+                </Frame>
+            </Body>
         </Layout>
         
     );
   };
+  
   export default Homework;
