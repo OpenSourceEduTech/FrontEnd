bff97acae7c4853642b1e75dee67328af259819a
youm <juliet0524@naver.com>
2023-04-15 22:14:26 +0900

    내 과제 _ 틀 추가

--- src/pages/Homework.js
+++ src/pages/Homework.js
@@ -2,49 +2,52 @@ import Layout from "../components/Layout";
 import styled from "styled-components";
 
 const Body = styled.div`
-display: flex;
-justify-content: space-evenly;
-align-items: center;
-position: fixed;
-top: 0%;
-width: 100vw;
-height: 100vh;
-
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  position: fixed;
+  top: 0%;
+  width: 100vw;
+  height: 100vh;
 `;
 const LeftCon = styled.div`
+  flex-direction: column;
+  display: flex;
+  position: flex;
+  width: 80%;
+  height: 100%;
 
-flex-direction: column;
-display:flex;
-position: flex;
-    width: 80%;
-    height:100%;
-  
-    background-color: #000000;
-    
-
+  background-color: #000000;
 `;
 const RightCon = styled.div`
-width:20%;
-height:100%;
-flex-direction: column;
-display:flex;
-background-color: pink;
-
-`
-
+  width: 20%;
+  height: 100%;
+  flex-direction: column;
+  display: flex;
+  background-color: pink;
+  align-items: center;
+`;
+const Mytask = styled.div`
+  width: 80%;
+  height: 20%;
+  justify-content: center;
+  background-color: white;
+  margin-top: 45%;
+  box-shadow: 0px 5px 9px -6px #000000;
+  border: 1px;
+`;
 const Homework = () => {
-    return (
-        <>
-        <Layout/>
-            <Body>
-   <LeftCon>
-   </LeftCon>
-   <RightCon>
+  return (
+    <>
+      <Layout />
+      <Body>
+        <LeftCon></LeftCon>
+        <RightCon>
+          <Mytask></Mytask>
+        </RightCon>
+      </Body>
+    </>
+  );
+};
 
-   </RightCon>
-            </Body>
-        </>
-    );
-  };
-  
-  export default Homework;
+export default Homework;
